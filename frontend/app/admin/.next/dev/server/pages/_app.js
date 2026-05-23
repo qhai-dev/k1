6637019a9aa4b97f3051aa59ac3112e4165f1302
qhai-dev <qhai-dev@outlook.com>
2026-05-23 22:33:30 +0800

    todo

--- frontend/app/admin/.next/dev/server/pages/_app.js
+++
@@ -1,8 +0,0 @@
-var R = require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/_app.js");
-R.c("server/chunks/ssr/[root-of-the-server]__a2723d53._.js");
-R.m(
-	"[project]/node_modules/.pnpm/next@16.0.10_@playwright+test@1.59.1_babel-plugin-react-compiler@1.0.0_react-dom@19.2.0_1fc69c9160eaa12b7c8dc80ee4bf043f/node_modules/next/app.js [ssr] (ecmascript)",
-);
-module.exports = R.m(
-	"[project]/node_modules/.pnpm/next@16.0.10_@playwright+test@1.59.1_babel-plugin-react-compiler@1.0.0_react-dom@19.2.0_1fc69c9160eaa12b7c8dc80ee4bf043f/node_modules/next/app.js [ssr] (ecmascript)",
-).exports;
